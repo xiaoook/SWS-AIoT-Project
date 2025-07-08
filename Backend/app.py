c0c9321a68aba7466dfb2847087e37733912f1da
Yao Xiao <scyyx10@nottingham.edu.cn>
2025-07-08 12:29:33 +0800

    feat: Add retrieve rounds

--- Backend/app.py
+++ Backend/app.py
@@ -29,5 +29,33 @@ def call_retrieve_games():
         "games": games
     }), 200
 
+@app.route('/games/<gid>/rounds', methods=['GET'])
+def call_retrieve_rounds(gid=None):
+    logger.debug(f'gid: {gid}')
+    # the circumstance that no gid provided
+    if gid is None:
+        logger.error('no gid provided')
+        return jsonify({
+            "status": "error",
+            "message": "gid is required"
+        }), 422
+
+    # retrieve rounds
+    rounds = retrieve_rounds(gid)
+
+    # The circumstance that no rounds found
+    if rounds == []:
+        logger.error('no rounds found')
+        return jsonify({
+            "status": "error",
+            "message": "Round not found"
+        }), 404
+    
+    logger.info(f'Found {len(rounds)} rounds')
+    return jsonify({
+        "status": "success",
+        "rounds": rounds
+    }), 200
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- Backend/core/dataManage.py
+++ Backend/core/dataManage.py
@@ -39,3 +39,26 @@ def retrieve_games(limit: int = 10) -> list | None:
     # make it a [dict]
     games = [dict(row) for row in results]
     return games
+
+def retrieve_rounds(gid: int) -> list | None:
+    logger.info(f"Retrieving rounds for game {gid}")
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_FILE)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT * FROM Round
+        WHERE gid = ? 
+        ORDER BY 
+            roundInGame ASC""", (gid,))
+        results = cur.fetchall()
+    except sqlite3.OperationalError as e:
+        logger.error(f"Error: {e}")
+        return None
+    finally:
+        if conn is not None:
+            conn.close()
+
+    rounds = [dict(row) for row in results]
+    return rounds
