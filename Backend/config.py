ba74b7f430bae0e866644287c438b66e96942090
Yao Xiao <scyyx10@nottingham.edu.cn>
2025-07-14 18:03:34 +0800

    chore: Update config

--- Backend/config.py
+++ Backend/config.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 # For app.run
-BACKEND_URL = '127.0.0.1'
-BACKEND_PORT = 5000
+BACKEND_URL = '0.0.0.0'
+BACKEND_PORT = 45678
 
 # For MQTT
-BROKER_URL = '127.0.0.1'
-BROKER_PORT = 45677
+BROKER_URL = '0.0.0.0'
+BROKER_PORT = 45679
 
 # For Database
 DB_FILE = Path.cwd() / 'Backend' / "data" / "data.db"
